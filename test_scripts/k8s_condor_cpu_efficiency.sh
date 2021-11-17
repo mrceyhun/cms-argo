d7f2b5dd345cd5b9a678f078305a0a29f92fbbe9
cuzunogl <ceyhunuzngl@gmail.com>
2021-11-17 16:08:24 +0100

    Add test script

--- test_scripts/k8s_condor_cpu_efficiency.sh
+++ test_scripts/k8s_condor_cpu_efficiency.sh
@@ -2,6 +2,14 @@
 # Create a site for the default period for each of the cms types
 #
 # This script is intended to be used as cron job in kubernetes.
+# Usage:
+#      ./k8s_condor_cpu_efficiency.sh OUTPUT_DIR PORT1 PORT2 LAST_N_DAYS
+# Args:
+#      1: OUTPUT_DIR, output eos directory
+#      2: PORT1, should be a NodePort in k8s, since it'll reach to Spark cluster
+#      3: PORT2, should be a NodePort in k8s, since it'll reach to Spark cluster
+#      4: LAST_N_DAYS, see python script
+#
 
 echo '=================================================================================================='
 echo '_.~"(_.~"(_.~"(_.~"(_.~"(   Condor CPU Efficiency cron job is starting   _.~"(_.~"(_.~"(_.~"(_.~"('
@@ -12,20 +20,32 @@ echo '==========================================================================
   echo "ERROR: Output path is not defined"
   exit 1
 }
+# Check output path is given
+[[ -z "$2" ]] && {
+  echo "ERROR: spark.driver.port is not defined"
+  exit 1
+}
+# Check output path is given
+[[ -z "$3" ]] && {
+  echo "ERROR: spark.driver.blockManager.port is not defined"
+  exit 1
+}
+[[ -z "$4" ]] && {
+  echo "ERROR: LAST_N_DAYS parameter is not defined"
+  exit 1
+}
 
 # Check env vars are set
 [[ -z "$MY_NODE_NAME" ]] && {
   echo "ERROR: MY_NODE_NAME is not defined"
   exit 1
 }
-[[ -z "$CMSMONIT_SPARK_SERVICE_PORT_PORT_0" ]] && {
-  echo "ERROR: CMSMONIT_SPARK_SERVICE_PORT_PORT_0 is not defined"
-  exit 1
-}
-[[ -z "$CMSMONIT_SPARK_SERVICE_PORT_PORT_1" ]] && {
-  echo "ERROR: CMSMONIT_SPARK_SERVICE_PORT_PORT_1 is not defined"
-  exit 1
-}
+
+# Set arguments as variables
+MAIN_OUTPUT_DIR="${1}"
+PORT1="${2}"
+PORT2="${3}"
+LAST_N_DAYS="${4}"
 
 # Kerberos
 keytab=/etc/condor-cpu-eff/keytab
@@ -51,8 +71,8 @@ spark_submit=/usr/hdp/spark-2.4/bin/spark-submit
 spark_confs=(
   --conf "spark.driver.bindAddress=0.0.0.0"
   --conf "spark.driver.host=${MY_NODE_NAME}"
-  --conf "spark.driver.port=${CMSMONIT_SPARK_SERVICE_PORT_PORT_0}"
-  --conf "spark.driver.blockManager.port=${CMSMONIT_SPARK_SERVICE_PORT_PORT_1}"
+  --conf "spark.driver.port=${PORT1}"
+  --conf "spark.driver.blockManager.port=${PORT2}"
   --conf "spark.driver.extraClassPath=${WDIR}/hadoop-mapreduce-client-core-2.6.0-cdh5.7.6.jar"
   --conf "spark.executor.memory=8g"
   --conf "spark.executor.instances=31"
@@ -60,14 +80,11 @@ spark_confs=(
   --conf "spark.driver.memory=4g"
 )
 
-MAIN_OUTPUT_DIR="${1}"
-LAST_N_DAYS="${2}"
-
 # cpu_eff
-OUTPUT_DIR="${1}/cpu_eff"
+OUTPUT_DIR="${MAIN_OUTPUT_DIR}/cpu_eff"
 # cpu_eff_outlier
 OUTPUT_DIR_OUTLIER="${OUTPUT_DIR}_outlier"
-CMS_TYPES=("analysis" "production" "folding@home" "test")
+CMS_TYPES=("test")
 
 for type in "${CMS_TYPES[@]}"; do
   SUBFOLDER=$(echo "$type" | sed -e 's/[^[:alnum:]]/-/g' | tr -s '-' | tr '[:upper:]' '[:lower:]')
